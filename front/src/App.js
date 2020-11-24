1b8ef7f2ad9dbba7a8d0a64fc28ce7821949a75c
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2020-11-24 02:56:57 +0200

    Home Page Done

--- front/src/App.js
+++ front/src/App.js
@@ -1,38 +1,30 @@
 import React, {Component}from 'react'
 
 import './App.css';
-import Search from './components/search/search';
-import Create from './components/create/create'
-import Category from './components/categories/categories'
-import Card from './components/card/card'
+import './components/card/Style.css'
+//import Search from './components/search/search';
+//import Create from './components/create/create'
+//import Category from './components/categories/categories' 
+//import Card from './components/card/allCard';
+import Home from '../src/components/Home/home'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   state={
     games:[]
   }
-  async componentDidMount(){
-    const response=await fetch("http://localhost:8001/games");
-    const result= await response.json();
-    //console.log("INDIE BITE LINE 11");
-   //console.log(result);
-   this.setState({games:result});
-   console.log(this.state.games)
-    
-  }
+  
   
   
   render(){
 
     return (
-      <div className="App">
-       <h1> Hello Indie Bite</h1>
-    <Search />
-    <Create />
-    <Category />
-    
-    <Card />
+      
+        < div className="App"  >
+        <Home />
+          
        </div>
+        
 
     )
   }
--- front/src/components/search/search.js
+++ front/src/components/search/search.js
@@ -1,4 +1,6 @@
 import React,{Component} from 'react';
+import '../card/Style.css';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 
@@ -28,17 +30,38 @@ class Search extends Component{
     render(){
         return(
             <div className="Search">
+
                 
-        <form onSubmit={this.searchFunction}>
-            <input type="text" name="searchInput" placeholder="search"  />
-            <input type="submit" name="submit"/>
-            
-       </form>
-       <ul>{this.state.game.map(g =>
-           <li key={g.toString} >Name: {g.name} <br></br>Rating: {g.rate}<br></br>Image: {g.imagepath}<br></br>Release Date: {g.releasdate}<br></br>Blog: {g.post}</li>
-            
-            )}
-            </ul>
+                
+                  <img src="./img/indie_bite-01.svg" alt="Indie Bite" className="logo" />
+                   <form onSubmit={this.searchFunction}>
+                    <input type="text" name="searchInput" className="search" placeholder="What are you looking for?"/>
+                    <button type="submit" name="submit" className="searchButton">
+                    <i className="fa fa-search"></i>
+                    </button>
+                   </form>
+                  
+                
+                
+         
+        
+                  <div>{this.state.game.map(g =>
+                     <div key={g.id} className="card">
+                       <img src="https://images.wallpaperscraft.com/image/night_city_aerial_view_night_192859_1024x768.jpg" />
+                         <div className="info">
+                         <h3>{g.name}</h3> <br></br>
+                         <h3> Rating: {g.rate}</h3><br></br>
+                         <p>Release Date: {g.releasdate}</p>
+                         </div>
+
+                        <div className="gameName">
+                        <h3>{g.name}</h3>
+                       </div>
+                    </div>
+                      )}
+                
+            </div>
+         
                 
             </div>
         )
